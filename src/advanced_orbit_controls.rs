6d2d05ce074fef7f311c07843bc79de84c8caabb
S4ndf1re <jan.kleinmann2000@gmail.com>
2025-07-01 08:57:30 +0200

    added fixed rotation to vr controls

--- src/advanced_orbit_controls.rs
+++ src/advanced_orbit_controls.rs
@@ -79,8 +79,8 @@ fn orbit(
     let delta_yaw = delta.x * camera_settings.yaw_speed;
 
     root.rotation = Quat::from_axis_angle(Vec3::Z, delta_pitch)
-        * Quat::from_axis_angle(Vec3::Y, delta_yaw)
-        * root.rotation;
+        * root.rotation
+        * Quat::from_axis_angle(Vec3::Y, delta_yaw);
 }
 
 impl Plugin for AdvancedOrbitControls {
