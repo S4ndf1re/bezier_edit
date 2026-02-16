e075d3d32c4485a6dcbaceb10d009480bd7cfaa2
Jan Kleinmann <jan.kleinmann@proton.me>
2026-02-16 10:53:23 +0100

    deactivated movement of root transform. Can only use alignment mode from now on

--- src/advanced_orbit_controls.rs
+++ src/advanced_orbit_controls.rs
@@ -131,6 +131,6 @@ fn move_root(
 impl Plugin for AdvancedOrbitControls {
     fn build(&self, app: &mut App) {
         app.init_resource::<CameraSettings>();
-        app.add_systems(Update, (orbit, move_root));
+        // app.add_systems(Update, (orbit, move_root));
     }
 }
